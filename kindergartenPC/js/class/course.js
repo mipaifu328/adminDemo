de2fbf96b0b5d4f413b99c89d924866f352ee60f
mipaifu328 <594757223@qq.com>
2018-05-18 15:35:57 +0800

    课程交互调整
    课程交互调整

--- kindergartenPC/js/class/course.js
+++ kindergartenPC/js/class/course.js
@@ -1,11 +1,34 @@
 $(function(){
 	//模拟后台数据
+	Mock.mock('/loadCourse',{
+		"list":[{
+			'name': '跳舞'
+		},{
+			'name': '唱歌'
+		},{
+			'name': '游戏'
+		},{
+			'name': '礼仪'
+		},{
+			'name': '讲故事'
+		},{
+			'name': '户外'
+		},{
+			'name': '手工'
+		},{
+			'name': '交流'
+		},{
+			'name': '活动'
+		},{
+			'name': '劳动'
+		}]
+	});
 	Mock.mock('/getCourse',{
 		"list":[
 			{
 			    id:1,
 			    title: '跳舞',
-			    start:  '2018/5/15 10:30',
+			    start:  '2018/5/15 10:20',
 			    end: '2018/5/15 11:30'
 			},
 			{
@@ -53,30 +76,56 @@ $(function(){
 		}
 	);
 	Mock.mock('/updateCourse',{});
-	/* initialize the external events
-    -----------------------------------------------------------------*/
-
-    $('#external-events .fc-event').each(function() {
-
-      // store data so the calendar knows to render an event upon drop
-      $(this).data('event', {
-        title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true // maintain when user navigates (see docs on the renderEvent method)
-      });
-
-      // make the event draggable using jQuery UI
-      $(this).draggable({
-        zIndex: 999,
-        revert: true,      // will cause the event to go back to its
-        revertDuration: 0  //  original position after the drag
-      });
-
-    });
-
-
-    /* initialize the calendar
-    -----------------------------------------------------------------*/
-
+	Mock.mock('/addCourse',{});
+	Mock.mock('/deleteCourse',{});
+	Mock.mock('/deleteEvent',{});
+	
+	//加载课程列表
+	loadCourse();
+	
+	//增加课程列表
+	$('.add-course').on('click',function(){
+		var text = $.trim($('.course-input').val());
+		if(text !== ''){
+			$.ajax({
+				url: '/addCourse',
+				type: 'post',
+				dataType: 'json',
+				data: {
+				},
+				success: function(data) { // 获取当前月的数据
+					var $newCourse = $('<div class="fc-event">'+ text +'</div>');
+					$('#external-events p').before($newCourse);
+					initDrag($newCourse);
+				},
+				error:function(e){
+					console.log(e);
+				}
+			});
+			
+		}
+	});
+	
+	//删除课程列表
+	$('#external-events').on('dblclick','.fc-event',function(){
+		var $this = $(this);
+		$.ajax({
+				url: '/deleteCourse',
+				type: 'post',
+				dataType: 'json',
+				data: {
+				},
+				success: function(data) { // 获取当前月的数据
+					$this.remove();
+				},
+				error:function(e){
+					console.log(e);
+				}
+			});
+	});
+	
+    
+	//初始化日历
     $('#calendar').fullCalendar({
 		defaultView: 'agendaWeek',
 		allDaySlot: false,
@@ -106,17 +155,22 @@ $(function(){
 						});
 					});
 					callback(events);
+				},
+				error:function(e){
+					console.log(e);
 				}
 			});
-		},
-		drop: function() {
-			
 		},
 		eventDrop: function(event, delta, revertFunc){
 			updateTime(event,delta);
 		},
 		eventResize: function(event, delta, revertFunc){
 			updateTime(event,delta);
+		},
+		eventClick: function(event, jsEvent, view ){
+			if(confirm('删除“”'+ event.title +'”课程吗?')){
+				deleteEvent(event._id);
+			}
 		}
 	});
     
@@ -128,9 +182,10 @@ $(function(){
      */
     function updateTime(event,delta){
     	var param = {
+    		'id': event._id,
     		'title': event.title,
-    		'start': event.start.format('YYYY-MM-DD HH:mm:ss'),
-    		'end': event.end.format('YYYY-MM-DD HH:mm:ss')
+    		'start': event.start ? event.start.format('YYYY-MM-DD HH:mm:ss') : '',
+    		'end': event.end ? event.end.format('YYYY-MM-DD HH:mm:ss') : ''
     	}
     	$.ajax({
 			url: '/updateCourse',
@@ -139,8 +194,82 @@ $(function(){
             data: param,
             success: function(data) { // 获取当前月的数据
                 alert(JSON.stringify(param));
-            }
+            },
+			error:function(e){
+				console.log(e);
+			}
+        });
+    }
+    
+    /**
+     * 加载课程列表
+     * @param {Object} event
+     * @param {Object} delta
+     */
+    function loadCourse(){
+    	$.ajax({
+			url: '/loadCourse',
+			type: 'post',
+            dataType: 'json',
+            data: {
+            	
+            },
+            success: function(data) {
+            	var html = '';
+				$.each(data.list, function(index,element) {
+					html += '<div class="fc-event">'+ element.name +'</div>';
+				});
+				$('#external-events p').before(html);
+				
+				//初始化课程拖拽事件
+				$('#external-events .fc-event').each(function() {
+					initDrag($(this));
+				});
+            },
+			error:function(e){
+				console.log(e);
+			}
         });
     }
 	
+	/**
+	 * 初始化课程拖拽事件
+	 * @param {Object} $obj 课程jquery对象
+	 */
+	function initDrag($obj){
+		 // store data so the calendar knows to render an event upon drop
+		$obj.data('event', {
+			title: $.trim($obj.text()), // use the element's text as the event title
+	        stick: true // maintain when user navigates (see docs on the renderEvent method)
+		});
+	
+		// make the event draggable using jQuery UI
+		$obj.draggable({
+			zIndex: 999,
+			revert: true,      // will cause the event to go back to its
+			revertDuration: 0  //  original position after the drag
+		});
+	}
+	
+	/**
+	 * 删除课程日程
+	 * @param {Object} eventId 日程id
+	 */
+	function deleteEvent(eventId){
+		$.ajax({
+			url: '/deleteEvent',
+			type: 'post',
+            dataType: 'json',
+            data: {
+            	id: eventId
+            },
+            success: function(data) {
+            	//删除成功
+            	$('#calendar').fullCalendar('refetchEvents'); //重新获取
+            },
+			error:function(e){
+				console.log(e);
+			}
+        });
+	}
 })
